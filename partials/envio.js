d326a47cdaeaeafcdcd3a13e10bae857ab886b77
al417146 <al417146@uji.es>
2025-11-16 00:41:52 +0100

    JS append

---
+++ partials/envio.js
@@ -0,0 +1,14 @@
+function sendMail() {
+    emailjs.send("service_d7uflbm", "template_b7y0p1r", {
+        nombre: nombre,
+        name: "EnvioDeDatosDesdeElForm",
+        email: email,
+        mensaje: mensaje,
+    }).then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            console.log('FAILED...', error);
+        },
+    );
+}
