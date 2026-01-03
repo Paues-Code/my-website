1a4182f878fcc7253a5aa297b57671b3d2a21c52
al417146 <al417146@uji.es>
2026-01-03 21:43:46 +0100

    Dos directorios borrados y fotos

--- partials/envio.js
+++ partials/envio.js
@@ -6,11 +6,13 @@ function sendMail() {
         mensaje: mensaje,
     }).then((response) => {
             console.log('SUCCESS!', response.status, response.text);
+            window.location.href = "https://paues-code.github.io/my-website/";
         },
         (error) => {
             console.log('FAILED...', error);
         },
     );
+    window.location.href = "https://paues-code.github.io/my-website/";
 }
 
 function closePage() {
